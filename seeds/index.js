60f5b5fe2bd3363f31880c3c919ad3b331605ec0
kejiantong <tong.ke@northeastern.edu>
2021-08-04 03:05:07 -0600

    finish 515

--- seeds/index.js
+++ seeds/index.js
@@ -16,7 +16,6 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
@@ -26,6 +25,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '610a47bd5a94c134572be9e2',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
